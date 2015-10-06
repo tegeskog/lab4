b01a9be181cf7fc8609d98df95a6c1d502b6eb7d
Tomas Tegeskog <tomas.tegeskog@gmail.com>
2015-10-06 08:47:26 +0200

    Revert "Raderade filen stringutility"
    This reverts commit ae6ce8fae3af8564b8af45088ea600dff2225316.

---
+++ Testing_lab4/StringUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_lab4
+{
+    class StringUtility
+    {
+        int x = 0;
+        double y = 0;
+
+
+
+    }
+}
